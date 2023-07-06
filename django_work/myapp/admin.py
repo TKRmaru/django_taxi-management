35210cee8fb497432d87ea1519c3322133db1baa
takuro <takuro.maruyama04@gmail.com>
2023-07-06 19:18:01 +0900

    管理者ページの更新

--- django_work/myapp/admin.py
+++ django_work/myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CarInformation, CustomerInformation, PlaceInformation, SalesRecord
 
-# Register your models here.
+admin.site.register(CarInformation)
+admin.site.register(CustomerInformation)
+admin.site.register(PlaceInformation)
+admin.site.register(SalesRecord)
